f5429678189008b21a8071f6fe613a5ca49b3ec5
Konstantin <konstantin.k.ed@gmail.com>
2016-08-16 17:39:58 +0100

    cleanup in update-parity

--- update-parity.sh
+++ update-parity.sh
@@ -3,7 +3,6 @@
 function up() {
 	echo "Getting $1 binaries from $2"
 
-	mkdir -p ./nodes/parity
 	curl -L $2 > "./nodes/parity/$1.zip"
 
 	if [[ $1 == win32-x64 ]]; then
@@ -11,10 +10,13 @@ function up() {
 	else
 		unzip -j "./nodes/parity/$1.zip" target/release/parity -d "./nodes/parity/$1" 
 	fi
-	
+
+	rm "./nodes/parity/$1.zip"
 }
 
+rm -rf ./nodes/parity
+mkdir -p ./nodes/parity
 
-up darwin-x64 https://gitlab.ethcore.io/Mirrors/parity/builds/452/artifacts/download
-up linux-x64  https://gitlab.ethcore.io/Mirrors/parity/builds/445/artifacts/download
-up win32-x64  https://gitlab.ethcore.io/Mirrors/parity/builds/453/artifacts/download
+up darwin-x64 https://gitlab.ethcore.io/Mirrors/parity/builds/1449/artifacts/download
+up linux-x64  https://gitlab.ethcore.io/Mirrors/parity/builds/1442/artifacts/download
+up win32-x64  https://gitlab.ethcore.io/Mirrors/parity/builds/1450/artifacts/download
